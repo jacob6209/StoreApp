121e6c24b3b3c703ef04200abaf647b81fd64785
Jac <ebrahimi_h2o@yahoo.com>
2023-03-14 13:21:44 +0330

    CRUD Operation On Class BaseView Done

--- api/views.py
+++ api/views.py
@@ -5,49 +5,123 @@ from .serializers import ProductSerializer, CategorySerializer
 from storeapp.models import Category, Product
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 from api import serializers
 
 # Create your views here.
 
-@api_view(['GET','POST'])
-def api_products(request):
-    if request.method == "GET":
+# # === >   Class BaseView  < =====
+
+class ApiProducts(APIView):
+    def get(self,request):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
-    if request.method == "POST":
+
+    def post(self,request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
-@api_view(['GET','PUT','DELETE'])
-def api_product(request, pk):
-    product = get_object_or_404(Product, id=pk)
-    if request.method == "GET":
+class ApiProduct(APIView):
+    def get(self,request,pk):
+        product = get_object_or_404(Product, id=pk)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    if request.method == "PUT":
-        serializer = ProductSerializer(product,data=request.data)
+
+    def put(self,request,pk):
+        product = get_object_or_404(Product, id=pk)
+        serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    if request.method == "DELETE":
+
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, id=pk)
         product.delete()
-        return Response({'status':'True'},status=status.HTTP_204_NO_CONTENT)
+        return Response({'status': 'Success'}, status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view()
-def api_categories(request):
-    categories = Category.objects.all()
-    serializer = CategorySerializer(categories, many=True)
-    return Response(serializer.data)
+class ApiCategories(APIView):
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer=CategorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 
-@api_view()
-def api_category(request, pk):
-    category = get_object_or_404(Category, category_id=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+class ApiCategory(APIView):
+    def get(self,request,pk):
+        category = get_object_or_404(Category, category_id=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    def put(self,request,pk):
+        category = get_object_or_404(Category, category_id=pk)
+        serializer=CategorySerializer(category, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self,request,pk):
+        product = get_object_or_404(Product, id=pk)
+        product.delete()
+        return Response({'status': 'Success'}, status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+
+
+# # === >   Fanction BaseView  < =====
+# @api_view(['GET','POST'])
+# def api_products(request):
+#     if request.method == "GET":
+#         products = Product.objects.all()
+#         serializer = ProductSerializer(products, many=True)
+#         return Response(serializer.data)
+#     if request.method == "POST":
+#         serializer = ProductSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+#
+# @api_view(['GET','PUT','DELETE'])
+# def api_product(request, pk):
+#     product = get_object_or_404(Product, id=pk)
+#     if request.method == "GET":
+#         serializer = ProductSerializer(product)
+#         return Response(serializer.data)
+#     if request.method == "PUT":
+#         serializer = ProductSerializer(product,data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+#     if request.method == "DELETE":
+#         product.delete()
+#         return Response({'status':'True'},status=status.HTTP_204_NO_CONTENT)
+
+
+
+# @api_view()
+# def api_categories(request):
+#     categories = Category.objects.all()
+#     serializer = CategorySerializer(categories, many=True)
+#     return Response(serializer.data)
+
+
+# @api_view()
+# def api_category(request, pk):
+#     category = get_object_or_404(Category, category_id=pk)
+#     serializer = CategorySerializer(category)
+#     return Response(serializer.data)
 
